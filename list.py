dd2342e76a5a9b37aa5271a85d28feeefc90f76c
Alf0nce <alfoncekngetich@gmail.com>
2025-03-22 20:59:24 +0300

    Updated Assignment

--- list.py
+++ list.py
@@ -1,2 +1,25 @@
-my_list=()
+my_list=[]
 print(my_list)
+my_list=[5,3,4,2,1]
+print(my_list)
+
+numbers=(10,20,30,40)
+my_list.append(numbers)
+print(my_list)
+
+my_list.insert(1,15)
+print(my_list)
+
+nums=50,60,70
+my_list.extend(nums)
+print(my_list)
+
+my_list.remove(70)
+print(my_list)
+
+my_list=[5, 15, 3, 4, 2, 1, 10, 20, 30, 40, 50, 60]
+my_list.sort()
+print(my_list)
+
+index=my_list.index(30)
+print(index)
